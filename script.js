2432ea72520ba428fa09cf50e2890be1a7fdddf7
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-01-14 14:01:04 +0530

    update

---
+++ script.js
@@ -0,0 +1,47 @@
+const userLocation = document.getElementById('userLocation');
+const userAccess = document.getElementById('userAccess');
+const val = document.getElementById('errorOutput1')
+const mapping = document.getElementById('map')
+
+
+function renderData(data){
+    console.log(data);
+    document.getElementById("lat_ash1").innerHTML = data.coord.lat
+    document.getElementById("lon_ash1").innerHTML = data.coord.lon
+    document.getElementById("location_ash").innerHTML = data.name
+    document.getElementById("lat_ash").innerHTML = data.coord.lat
+    document.getElementById("lon_ash").innerHTML = data.coord.lon
+    document.getElementById("time_ash").innerHTML = data.timezone
+    document.getElementById("winds_ash").innerHTML = data.wind.speed
+    document.getElementById("pres_ash").innerHTML = data.main.pressure
+    document.getElementById("humi_ash").innerHTML = data.main.humidity
+    document.getElementById("windd_ash").innerHTML = data.wind.deg
+    document.getElementById("feels_like").innerHTML = data.main.feels_like
+    mapping.src = `https://maps.google.com/maps?q=${data.coord.lat}, ${data.coord.lon}&output=embed`
+}
+
+//for fetching and all
+function submitData(userLocation) {
+
+     console.log(userLocation);
+     console.log(userAccess);
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${userLocation.coords.latitude}&lon=${userLocation.coords.longitude}&appid=41f0381fedd924492178a2ae8c53edfd`
+      fetch(url)
+      .then((response) => response.json())
+      .then((data) => renderData(data))
+      .catch((error) => console.log(error))
+}
+
+
+//how to Gelocation
+
+const successCallback = (position) => {
+    submitData(position)
+    console.log(position);
+  };
+  
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+  
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
